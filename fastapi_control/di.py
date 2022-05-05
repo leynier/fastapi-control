dc8f8354e6de61a05e7259cfe076c540abd6ec62
Leynier Gutiérrez González <leynier41@gmail.com>
2022-05-05 03:45:53 -0400

    fix: implement custom Container for avoid kink bug

--- fastapi_control/di.py
+++ fastapi_control/di.py
@@ -1,16 +1,34 @@
-from typing import Any, Callable, Optional, Type, TypeVar
+from typing import Any, Callable, Optional, Type, TypeVar, Union
 
+from kink import Container
 from kink import inject as kink_inject
 
 T = TypeVar("T")
 
 
+class _Container(Container):
+    """
+    This is a hack to combine the alias and the use_factory params
+    because in the kink default Container class when alias param
+    is used, the use_factory param is ignored.
+    """
+
+    def __getitem__(self, key: Union[str, Type]) -> Any:
+        if key in self._aliases:
+            key = self._aliases[key][0]
+
+        return Container.__getitem__(self, key)
+
+
+_di = _Container()
+
+
 def inject(alias: Optional[Type[Any]] = None) -> Callable[[Type[T]], Type[T]]:
     def decorator(cls: Type[T]) -> Type[T]:
         wrapper = (
-            kink_inject(use_factory=True)
+            kink_inject(use_factory=True, container=_di)
             if alias is None
-            else kink_inject(alias=alias, use_factory=True)
+            else kink_inject(alias=alias, use_factory=True, container=_di)
         )
         return wrapper(cls)
 
